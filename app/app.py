b6aaef840df03627a7f9eb21f875fd8ae132ea64
yegerless <Sergey.sys@mail.ru>
2024-03-30 10:31:54 +0300

    end working

--- app/app.py
+++ app/app.py
@@ -1,24 +1,35 @@
 import pandas as pd
 import streamlit as st
+import matplotlib.pyplot as plt
 
-df = pd.read_csv('./data/train.csv')
+df1 = pd.read_csv('./data/train.csv')
+df2 = pd.read_csv('./data/test.csv')
+
+df = pd.concat([df1, df2])
 
 passengers_cnt = df['PassengerId'].count()
-survived_passengers_count = df.loc[df['Survived'] == 1, 'PassengerId'].count()
+average_passengers_age = f'{df['Age'].mean():.2f} years'
 
 
-st.write("Hello! There my first streamlit app for 'Start in Data Science' bootcamp.")
+fig, ax = plt.subplots()
+labels = [f'Class {i}' for i in df['Pclass'].unique()]
+sizes = df.groupby('Pclass')['PassengerId'].count()
+ax.pie(x=sizes, labels=labels, autopct='%.0f%%',
+       textprops={'size': 'smaller'}, radius=1)
 
-left_column, right_column = st.columns(2)
+st.title('Titanic dataset')
 
-with left_column:
-    card1 = st.container(height=250, border=True)
-    card1.title('Count of titanic passengers')
-    card1.text(passengers_cnt)
+st.write("Hello! There is my first streamlit app for 'Start in Data Science' bootcamp.")
 
-with right_column:
-    card2 = st.container(height=250, border=True)
-    card2.title('Count of survived titanic passengers')
-    card2.text(survived_passengers_count)
+column1, column2 = st.columns(2)
+
+column1.metric('Count of titanic passengers', passengers_cnt)
+column2.metric('Average age of titanic passengers', average_passengers_age)
+
+st.bar_chart(data=df, x='Sex', y='PassengerId')
+
+st.pyplot(fig)
+
+st.image(image='https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Titanic_in_color.png/2560px-Titanic_in_color.png')
 
 st.dataframe(df)
